9a08b8f413561de492df30095f6a2a63719a9ab7
Chi-kwan Chan <chanc@arizona.edu>
2020-12-06 02:08:33 -0700

    Introduce the Nullify function
    nullify(x, v) turns the vector v into a null vector v' at x by
    rescaling v's spatial components.

---
+++ mod/fadge/utils.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2020 Chi-kwan Chan
+# Copyright (C) 2020 Steward Observatory
+#
+# This file is part of fadge.
+#
+# Fadge is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Fadge is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+# License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with fadge.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from jax       import numpy as np
+from jax.numpy import dot
+
+
+def Nullify(metric, p=1):
+
+    assert p > 0
+
+    def nullify(x, v): # closure on `p`
+        g = metric(x)
+        A = v[:p] @ g[:p,:p] @ v[:p]
+        B = v[p:] @ g[p:,:p] @ v[:p]
+        C = v[p:] @ g[p:,p:] @ v[p:]
+        D = - A / (B + np.sqrt(B * B - A * C)) # normalization
+        return np.concatenate((v[:p], v[p:] * D))
+
+    return nullify
