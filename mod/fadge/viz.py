7e685dff6412a0a08d3fcfd840a4097b1fd8886b
Chi-kwan Chan <chanc@arizona.edu>
2022-01-17 22:10:35 -0700

    Support plotting face-on horizons

--- mod/fadge/viz.py
+++ mod/fadge/viz.py
@@ -25,6 +25,7 @@ def KSHorizons(aspin, n=None):
     if n is None:
         n = 46 if aspin <= 1 else 721
 
+    phi   = np.linspace(-np.pi, np.pi, 2*(n-1)+1)
     theta = np.linspace(0, np.pi, n)
     ct    = np.cos(theta)
     st    = np.sin(theta)
@@ -41,15 +42,10 @@ def KSHorizons(aspin, n=None):
         return R[np.isfinite(R)], z[np.isfinite(z)]
 
     def plot2(ax, R, z, *args, **kwargs):
-        ax.plot(R,  z, *args, **kwargs)
+        ax.plot( R, z, *args, **kwargs)
         ax.plot(-R, z, *args, **kwargs)
 
-    def horizons(ax=None, color=None, eh=True, es=True, **kwargs):
-        if ax is None:
-            from matplotlib import pyplot as plt
-            fig, ax = plt.subplots(1,1,**kwargs)
-            ax.set_aspect('equal')
-
+    def plotedgeon(ax, color=None, eh=True, es=True):
         p = ax.plot([-aspin, aspin], [0,0], ':', color=color)
         c = p[0].get_color()
         ax.scatter([-aspin, aspin], [0,0], color=c, label=f'a={aspin}')
@@ -68,5 +64,34 @@ def KSHorizons(aspin, n=None):
             if eh:
                 plot2(ax, *Rz(rhp), color=c)
                 plot2(ax, *Rz(rhm), color=c)
+
+    def xy(r):
+        R = max(np.sqrt(r * r + aa), 1e-4)
+        return R * np.sin(phi), R * np.cos(phi)
+
+    def plotfaceon(ax, color=None, eh=True, es=True):
+        p = ax.plot(*xy(0.0), color=color, linewidth=4, label=f'a={aspin}')
+        c = p[0].get_color()
+
+        if abs(aspin) > 1:
+            if es:
+                ax.plot(*xy(2), color=c, linewidth=1)
+        else:
+            if es:
+                ax.plot(*xy(2), color=c, linewidth=1)
+            if eh:
+                ax.plot(*xy(rhp), color=c)
+                ax.plot(*xy(rhm), color=c)
+
+    def horizons(ax=None, color=None, faceon=False, eh=True, es=True, **kwargs):
+        if ax is None:
+            from matplotlib import pyplot as plt
+            fig, ax = plt.subplots(1,1,**kwargs)
+            ax.set_aspect('equal')
+
+        if faceon:
+            plotfaceon(ax, color, eh, es)
+        else:
+            plotedgeon(ax, color, eh, es)
 
     return horizons
