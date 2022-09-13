9eed778d38b030dcbfe620f7d19da7add3653cd5
Tintin Nguyen <87962312+dcgvr@users.noreply.github.com>
2022-09-12 22:38:53 -0700

    Update __main__.py

--- mod/fadge/__main__.py
+++ mod/fadge/__main__.py
@@ -45,13 +45,13 @@ def fadge():
 
 @click.argument('aspin',       type=float)
 @click.argument('inclination', type=float)
-@click.argument('PA',          type=float)
+@click.argument('pa',          type=float)
 
-def grrt(aspin, eps, setup, PA, alpha0, beta0, full, atol, inclination):
+def grrt(aspin, eps, setup, pa, alpha0, beta0, full, atol, inclination):
     print( "Fadge: general relativistic ray tracing")
     print(f"    aspin       = {aspin:.2f}")
     print(f"    inclination = {inclination:g}")
-    print(f"    inclination = {PA:g}")
+    print(f"    inclination = {pa:g}")
 
     ns = GRRT(
         aspin, ind='time',
@@ -68,8 +68,8 @@ def grrt(aspin, eps, setup, PA, alpha0, beta0, full, atol, inclination):
     elif setup == 'axis':
         if beta0 != 0.0:
             print('WARNING: set_axis() does not support setting `beta0`; ignore')
-        ns.set_axis(16, 1024, PA = PA, alpha0=alpha0)
-        out = f'axis_a{aspin:.2f}_i{inclination:g}_pa{PA:g}.h5'
+        ns.set_axis(16, 1024, PA = pa, alpha0=alpha0)
+        out = f'axis_a{aspin:.2f}_i{inclination:g}_pa{pa:g}.h5'
     else:
         raise ValueError(f'Unknown setup "{setup}".')
 
