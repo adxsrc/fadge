bb1e4611e0ba23f0a7586641befbd09d177cd470
Chi-kwan Chan <chanc@arizona.edu>
2022-09-12 23:48:24 -0700

    Bump fadge version to 0.1.6

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fadge',
-    version='0.1.5',
+    version='0.1.6',
     url='https://github.com/adxsrc/fadge',
     author='Chi-kwan Chan',
     author_email='chanc@arizona.edu',
